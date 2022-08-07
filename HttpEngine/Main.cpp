7bb28355c6bff1f7898115689f02c0b0028273fc
ubuntu <ubuntu@localhost.localdomain>
2022-08-07 11:54:47 +0800

    modify note

--- HttpEngine/Main.cpp
+++ HttpEngine/Main.cpp
@@ -9,7 +9,7 @@
 #include "SocketChannel.h"
 
 int main(int argc, char *argv[]){
-    std::cout<<"main started, tangao1234."<<std::endl;
+    std::cout<<"main started, tangsz."<<std::endl;
     std::shared_ptr<Engine> engine = std::make_shared<Engine>(9999,10);
     engine->Start();
     return  0;
